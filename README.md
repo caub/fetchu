30e0a2bce4f33ec98aecf0b13785b37b8d92a62a
caub <cyril.auburtin@gmail.com>
2017-12-01 09:54:26 +0100

    typo

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 browser
 ```js
 import fetcho from 'fetcho';
-fetch('https://httpbin.org/get?test=foo')
+fetcho('https://httpbin.org/get?test=foo')
 	.then(console.log)
 ```
 
 node
 ```js
 const fetcho = require('fetcho');
-fetch('https://httpbin.org/get?test=foo')
+fetcho('https://httpbin.org/get?test=foo')
 	.then(console.log)
 ```
--- test/index.js
+++ test/index.js
@@ -3,11 +3,14 @@ const assert = require('assert');
 const fetcho = require('..');
 
 nock('http://o.xo')
-	.get('/test')
-	.reply(200, {ok: 1})
+	.get('/test').reply(200, {ok: 1})
+	.get('/foo').reply(200, {ok: 2});
 
 fetcho('http://o.xo/test')
 	.then(data => assert.deepEqual(data, {ok: 1}));
+
+fetcho('http://o.xo/foo')
+	.then(data => assert.deepEqual(data, {ok: 2}));
 
 fetcho('http://o.xo/404')
 	.catch(err => assert(err));
