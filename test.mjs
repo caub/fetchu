b25ea671cb5797f87b3dadf8e2524f6b56613b10
caub <cyril.auburtin@gmail.com>
2018-10-14 20:52:27 +0200

    add puppeteer tests

--- test.mjs
+++ test.mjs
@@ -1,9 +1,40 @@
 import { deepEqual as eq } from 'assert';
 import fetchu from './fetchu.mjs';
+import puppeteer from 'puppeteer';
 
 (async () => {
-	const r = await fetchu('http://httpbin.org/post', { method: 'POST', body: { ok: 3 } });
-	eq(JSON.parse(r.data), { ok: 3 });
-	console.log('ok');
+	let browser, page;
+	try {
+		// test fetchu.mjs
+		const r = await fetchu('http://httpbin.org/post', { method: 'POST', body: { ok: 3 } });
+		eq(JSON.parse(r.data), { ok: 3 });
+
+		// test fetchu.js on browser
+		browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+		page = await browser.newPage();
+		page.on('console', msg => console.log(msg.text()));
+		page.on('error', err => console.error('error', err));
+		page.on('pageerror', err => console.error('pageerror', err));
+		await page.addScriptTag({
+			content: `import fetchu from 'https://unpkg.com/fetchu';
+fetchu('https://cors-anywhere.herokuapp.com/http://example.com').then(d => {
+	window.__shortHtml = d.slice(0, 30);
+});
+`,
+			type: 'module'
+		});
+		await page.waitForFunction('window.__shortHtml');
+		eq(await page.evaluate(() => __shortHtml), `<!doctype html>
+<html>
+<head>
+`);
+		console.log('ok');
+
+	} catch (e) {
+		console.error(e)
+	}
+
+	await page.close();
+	await browser.close();
+
 })()
-	.catch(console.error)
